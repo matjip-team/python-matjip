a7c41666f9e2dfd93151faecc0d3d7be7d390c11
xxx0209 <makelotus0209@gmail.com>
2026-02-24 11:01:09 +0900

    수정

--- app/api/user.py
+++ app/api/user.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends
 from app.services.auth_service import get_current_user_optional
 
-router = APIRouter(prefix="/user", tags=["User"])
+router = APIRouter(prefix="/api/fastapi/user", tags=["User"])
 
 @router.get("/me")
 def get_me(user=Depends(get_current_user_optional)):
